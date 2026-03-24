11a684c2ab1b3c21cb5aed7ec1dcea368d8345da
Danisss30 <133545368+Danisss30@users.noreply.github.com>
2026-03-23 20:56:11 -0600

    Change hardcoded loop limit to user input
    Modified the program to take user input for the number of rows.

--- Hoja3/Eje6.cpp
+++ Hoja3/Eje6.cpp
@@ -1,10 +1,15 @@
+//Daniela Nohemy Divas Carrillo
 #include <iostream>
 using namespace std;
 
 int main() {
+    int n;
+    cout << "Ingrese un numero: ";
+    cin >> n;
+
     int i = 1;
 
-    while (i <= 3) {
+    while (i <= n) {
         int j = 1;
 
         while (j <= i) {
